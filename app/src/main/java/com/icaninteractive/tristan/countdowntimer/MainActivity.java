07e8ac8ae5d69bd411b3903071be58d3e3e569b6
trist <tristanwilson111@gmail.com>
2017-07-18 12:14:30 -0400

    Added Progress Wheel from Github Library, needs adjusting

--- app/src/main/java/com/icaninteractive/tristan/countdowntimer/MainActivity.java
+++ app/src/main/java/com/icaninteractive/tristan/countdowntimer/MainActivity.java
@@ -3,14 +3,19 @@ package com.icaninteractive.tristan.countdowntimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.todddavies.components.progressbar.ProgressWheel;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ProgressWheel pw = (ProgressWheel) findViewById(R.id.pw_spinner);
 
-
+        pw.startSpinning();
+        //pw.stopSpinning();
+        //pw.incrementProgress();
 
 
     }
